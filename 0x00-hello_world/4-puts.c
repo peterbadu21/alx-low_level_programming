5434535c25444f190bd6231512f6820d20d1dfbb
peterbadu21 <peterbadu21@gmail.com>
2023-05-21 13:20:48 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a file that prints a line with puts
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
